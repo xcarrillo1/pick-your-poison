2f9eca6f3de8266bbf95d6e797abcacf44a1699c
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-09-15 10:45:44 -0400

    displayed name and category

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-const $getDrinkBtn = $("getDrinkBtn");
+let drinkData;
+const $getDrinkBtn = $("#getDrinkBtn");
 const $mainContent = $("main");
+const $name = $("#name");
+const $category = $("#category");
+const $ingredients = $("#ingredients");
+const $instructions = $("#instructions");
+
+
+
+function getDrink(event) {
+    event.preventDefault();
+    $.ajax({
+        url: "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    }).then(
+        (data) => {
+            console.log(data);
+            drinkData = data;
+            rendor(); 
+        }
+    );
+}
+
+function rendor() {
+    $name.text(drinkData.drinks[0].strDrink);
+    $category.text(drinkData.drinks[0].strCategory);
+    $ingredients.text(drinkData.drinks[0].strIngredient1);
+    $instructions.text(drinkData.drinks[0].strInstructions);
+};
+
+// event listener (button)
+$getDrinkBtn.on("click", getDrink)

191d7aa831e467819490b5709b55eb8412efc2c0
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-09-19 15:28:23 -0400

    ingredients w/o previous

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // alert("By clicking 'OK' you are confirming that you are 21 or older!");
 
 let drinkData;
+let allIng;
 const $getDrinkBtn = $("#getDrinkBtn");
 const $mainContent = $("main");
 const $name = $("#name");
@@ -16,8 +17,9 @@ function getDrink(event) {
         url: "https://www.thecocktaildb.com/api/json/v1/1/random.php"
     }).then(
         (data) => {
-            console.log(data);
+            // console.log(data);
             drinkData = data;
+            $ingredients.empty();
             rendor();
         }
     );
@@ -27,13 +29,13 @@ function getDrink(event) {
 function rendor() {
     for (let i=1; i<16; i++) {
         let allIng = drinkData.drinks[0][`strIngredient${i}`]
-        console.log(i);
-        console.log(allIng);
+        // console.log(i);
+        // console.log(allIng);
         $ingredients.append(allIng);
     }
     $name.text(drinkData.drinks[0].strDrink);
     $category.text(drinkData.drinks[0].strCategory);
-    $ingredients.text(drinkData.drinks[0].strIngredient)
+    $ingredients.text(drinkData.drinks[0].strIngredient);
     $instructions.text(drinkData.drinks[0].strInstructions);
     $poster.attr("src", drinkData.drinks[0].strDrinkThumb);
 };

f6db094278fab9bb485add1c90eaef0b19615781
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-09-19 12:54:31 -0400

    ingredients

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const $instructions = $("#instructions");
 const $poster = $("#poster");
 
 
-
 function getDrink(event) {
     event.preventDefault();
     $.ajax({
@@ -19,20 +18,27 @@ function getDrink(event) {
         (data) => {
             console.log(data);
             drinkData = data;
-            rendor(); 
+            rendor();
         }
     );
 }
 
 
-
 function rendor() {
+    for (let i=1; i<16; i++) {
+        let allIng = drinkData.drinks[0][`strIngredient${i}`]
+        console.log(i);
+        console.log(allIng);
+        $ingredients.append(allIng);
+    }
     $name.text(drinkData.drinks[0].strDrink);
     $category.text(drinkData.drinks[0].strCategory);
-    $ingredients.text(drinkData.drinks[0].strIngredient1)
+    $ingredients.text(drinkData.drinks[0].strIngredient)
     $instructions.text(drinkData.drinks[0].strInstructions);
     $poster.attr("src", drinkData.drinks[0].strDrinkThumb);
 };
+
+
 
 // event listener (button)
 $getDrinkBtn.on("click", getDrink)
